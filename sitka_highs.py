81d310302ed69d38094b59a790eea9cdb22d0b1e
space-aliens <vaibhav.choursiya.soc@gmail.com>
2023-10-08 16:06:01 +0530

    Revert "error"
    This reverts commit d516d0b5d12cb4cbf93c8ed6c379d8d0d554c924.

--- sitka_highs.py
+++ sitka_highs.py
@@ -29,7 +29,7 @@ for row in reader:
 # Plot the high temperatures
 plt.style.use(plt.style.available[16])
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red', linewidth=3)
+ax.polt(dates, highs, color='red', linewidth=1.5)
 
 # Format plot
 ax.set_title("Daily High Temperatures, 2021", fontsize=24)
