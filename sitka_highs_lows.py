abc80124e3621a467bc92e4aa64b07bca25f9743
space-aliens <vaibhav.choursiya.soc@gmail.com>
2023-10-11 17:59:14 +0530

    automate station name and index.

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -13,8 +13,22 @@ lines = path.read_text().splitlines()
 # reader object convert list items into individual list.
 reader = csv.reader(lines)
 
+TMAX = 0
+TMIN = 0
+station_name = ''
+
 # it return a next list from reader object.
 header_row = next(reader)
+
+for index, column_value in  enumerate(header_row):
+    if column_value == 'TMAX':
+        TMAX += index
+    elif column_value == 'TMIN':
+        TMIN += index
+    elif column_value == 'NAME':
+        station_name = column_value
+
+
 
 # Extract high temperatures and date.
 highs, lows, dates = [], [], []
@@ -41,7 +55,7 @@ ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 
 # Format plot
-ax.set_title("Daily High Temperatures 2021", fontsize=24)
+ax.set_title(f"Daily High Temperatures of {station_name}", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
