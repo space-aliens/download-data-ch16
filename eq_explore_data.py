482ff78e83268bfe835b58324819842c0c7eae73
space-aliens <vaibhav.choursiya.soc@gmail.com>
2023-10-12 15:00:56 +0530

    refactoring

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -12,18 +12,18 @@ all_eq_data = json.loads(contents)
 all_eq_dicts = all_eq_data['features']
 
 # datasets
-mags, lons, lats, eq_titles = [], [], [], []
+mags, lons, lats, eq_titles = [eq_dict['properties']['mag'] for eq_dict in all_eq_dicts], [eq_dict["geometry"]["coordinates"][0] for eq_dict in all_eq_dicts], [eq_dict["geometry"]["coordinates"][1] for eq_dict in all_eq_dicts], [eq_dict['properties']['title'] for eq_dict in all_eq_dicts]
 
-for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict["geometry"]["coordinates"][0]
-    lat = eq_dict["geometry"]["coordinates"][1]
-    eq_title = eq_dict['properties']['title']
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     lon = eq_dict["geometry"]["coordinates"][0]
+#     lat = eq_dict["geometry"]["coordinates"][1]
+#     eq_title = eq_dict['properties']['title']
 
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    eq_titles.append(eq_title)
+#     mags.append(mag)
+#     lons.append(lon)
+#     lats.append(lat)
+#     eq_titles.append(eq_title)
 
 title =  'Global Earthquakes'
 fig = px.scatter_geo(lat=lats, lon=lons, title=title, size=mags,
